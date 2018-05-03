d44d0a5cbcc077e1c0ea28fd1a4c7e9afaa5a43e
Gerardo Belot <gbelot2003@hotmail.com>
2018-05-02 22:15:33 -0600

    agregando paquete de auth

--- src/components/authentication/Login.vue
+++ src/components/authentication/Login.vue
@@ -2,7 +2,7 @@
 	<div class="row">
 		<div class="col-md-6">
 			<div class="panel panel-default">
-				
+
 				<div class="form-group">
 					<input type="email"
 							v-model="email"
@@ -15,10 +15,11 @@
 							v-model="password"
 							class="form-control"
 							placeholder="Password">
-				</div>				
+				</div>
+          <p>user: hstanton@example.com</p>
+          <p>pass: secret</p>
 
-
-				<button class="btn btn-success">Login</button>
+				<button class="btn btn-success" @click="login">Login</button>
 			</div>
 		</div>
 		<pre>{{ $data }}</pre>
@@ -32,7 +33,31 @@
 				email: '',
 				password: ''
 			}
-		}
-	}	
+		},
+    methods:{
+		  login () {
+		    let data = {
+		      client_id: 2,
+          client_secret: 'j0KeeM8QIJor0GI5AVxsY5nhEpOhdm06BxQA97Sw',
+          grant_type: 'password',
+          username: this.email,
+          password: this.password,
+
+        };
+		      this.$http.post("http://localhost/oauth/token", data)
+            .then(resp => {
+              console.log(resp);
+              this.$auth.setToken(resp.body.access_token, resp.body.expires_in + Date.now());
+              this.email = '';
+              this.password = '';
+            }, resp => {
+              this.email = '';
+              this.password = '';
+              console.log(resp);
+              alert(resp.statusText)
+            });
+      }
+    },
+	}
 
 </script>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Router from './routes';
+import Auth from './packages/auth/auth.js';
+
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+Vue.use(Auth);
 
 new Vue({
   el: '#app',
   render: h => h(App),
   router: Router
-})
+});
